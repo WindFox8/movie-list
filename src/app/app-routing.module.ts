c66e1fd027863fe4a4977134eb39f03d2cb08648
WindFox8 <rafael_correr@hotmail.com>
2024-07-17 20:32:50 -0300

    feat/movies list add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { GenreContentComponent } from './components/genre-content/genre-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'genre/:id', component: GenreContentComponent },
+  // Outras rotas...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/genre-content/genre-content.component.ts
+++ src/app/components/genre-content/genre-content.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TmdbApiService } from 'src/app/services/tmdb-api.service';
 
 @Component({
   selector: 'app-genre-content',
   templateUrl: './genre-content.component.html',
   styleUrls: ['./genre-content.component.sass']
 })
-export class GenreContentComponent {
+export class GenreContentComponent implements OnInit {
+  movies: any[] = [];
+  genreId!: number;
 
+  constructor(
+    private route: ActivatedRoute,
+    private tmdbApiService: TmdbApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.genreId = +params['id'];
+      this.loadMovies();
+    });
+  }
+
+  loadMovies(): void {
+    this.tmdbApiService.getMoviesByCategory(this.genreId).subscribe(data => {
+      this.movies = data.results;
+    }, error => {
+      console.error('Erro ao buscar filmes por gênero:', error);
+    });
+  }
 }
