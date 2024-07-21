4f144c6ae79a42209403658f557ce138a934ae05
WindFox8 <rafael_correr@hotmail.com>
2024-07-20 23:05:49 -0300

    feat/searchBar mechanics add

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TmdbApiService } from 'src/app/services/tmdb-api.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-bar.component.sass']
 })
 export class SearchBarComponent {
+  query: string = '';
+  movies: any[] = [];
 
+  constructor(
+    private tmdbApiService: TmdbApiService,
+    private router: Router
+  ) {}
+
+  onSearch(event: any): void {
+    const query = event.target.value.trim();
+    if (query.length > 0) {
+      this.tmdbApiService.searchMovies(query).subscribe(data => {
+        this.movies = data.results;
+      });
+    } else {
+      this.movies = [];
+    }
+  }
+
+  goToMovie(id: number): void {
+    this.movies = [];
+    this.query = '';
+    this.router.navigate(['/movie', id]);
+  }
 }
